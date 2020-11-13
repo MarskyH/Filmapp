2a9fab8b9d35934b1d865465d17fa44540e99e25
Marcus Loureiro <marcushuriel08@hotmail.com>
2020-11-13 08:09:36 -0300

    Icon do app adicionado

--- app/src/main/java/com/example/filmapp/Login/LoginActivity.kt
+++ app/src/main/java/com/example/filmapp/Login/LoginActivity.kt
@@ -1,6 +1,17 @@
 package com.example.filmapp.Login
 
-import androidx.appcompat.app.AppCompatActivity
 
-class LoginActivity: AppCompatActivity() {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.filmapp.R
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_login)
+
+
+
+    }
 }
