abf105ad4cabba1012aa70c6be616b6a3afbe26c
MatheusLeite08 <matheusleite08@gmail.com>
2020-11-17 18:32:32 -0300

    Atualizando

---
+++ app/src/main/java/com/example/filmapp/home/AgendaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.filmapp.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.filmapp.R
+
+class AgendaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda)
+    }
+}
