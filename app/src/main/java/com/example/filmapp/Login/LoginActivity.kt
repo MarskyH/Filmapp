7c2fbf0131a2693ef3b0e8d3346b04d0786119bf
Marcus Loureiro <70539396+MarcusLoureiro@users.noreply.github.com>
2020-11-19 08:09:01 -0300

    Revert "Adicionando o um cardView e a implementação das tabs fragments"

--- app/src/main/java/com/example/filmapp/Login/LoginActivity.kt
+++ app/src/main/java/com/example/filmapp/Login/LoginActivity.kt
@@ -1,12 +1,9 @@
 package com.example.filmapp.Login
 
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.filmapp.R
-import com.example.filmapp.Series.Ui.SerieSelectedActivity
-import kotlinx.android.synthetic.main.layout_login.*
 
 
 class LoginActivity : AppCompatActivity() {
@@ -14,10 +11,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_login)
 
-        imageView.setOnClickListener{
-            val intent = Intent(this, SerieSelectedActivity::class.java)
-            startActivity(intent)
-        }
+
 
     }
 }
