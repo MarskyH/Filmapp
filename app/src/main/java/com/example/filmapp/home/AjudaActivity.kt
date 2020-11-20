930c8dc91cc632708d63ab3d7069cd7582d914be
MatheusLeite08 <matheusleite08@gmail.com>
2020-11-20 14:20:56 -0300

    Layouts Home Versão 1

--- app/src/main/java/com/example/filmapp/home/AjudaActivity.kt
+++ app/src/main/java/com/example/filmapp/home/AjudaActivity.kt
@@ -1,12 +1,32 @@
 package com.example.filmapp.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.filmapp.R
+import kotlinx.android.synthetic.main.activity_ajuda.*
 
 class AjudaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ajuda)
+
+        setSupportActionBar(toolbarAjudaPage)
+
+        toolbarAjudaPage.setNavigationOnClickListener {
+            callHome()
+        }
+    }
+
+    //Usado para add o Menu a Toolbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_configuracoes_apenas, menu)
+        return true
+    }
+
+    fun callHome(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/filmapp/home/DescubraActivity.kt
+++ app/src/main/java/com/example/filmapp/home/DescubraActivity.kt
@@ -1,12 +1,34 @@
 package com.example.filmapp.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.filmapp.R
+import kotlinx.android.synthetic.main.activity_descubra.*
+import kotlinx.android.synthetic.main.activity_historico.*
 
 class DescubraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_descubra)
+
+        setSupportActionBar(toolbarDescubraPage)
+
+        toolbarDescubraPage.setNavigationOnClickListener {
+            callHome()
+        }
+
+    }
+
+    //Usado para add o Menu a Toolbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_configuracoes_apenas, menu)
+        return true
+    }
+
+    fun callHome(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
