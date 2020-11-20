1cc7a9a5d6d11426b1da0ac7d9afa410369cffe7
Marcus Loureiro <marcushuriel08@hotmail.com>
2020-11-20 14:49:50 -0300

    Subindo telas e fragments das séries

--- app/src/main/java/com/example/filmapp/Login/LoginActivity.kt
+++ app/src/main/java/com/example/filmapp/Login/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.filmapp.Login
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.filmapp.R
+import com.example.filmapp.Series.Ui.SerieSelectedActivity
+import kotlinx.android.synthetic.main.layout_login.*
 
 
 class LoginActivity : AppCompatActivity() {
@@ -11,7 +14,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_login)
 
-
+        imageView.setOnClickListener{
+            val intent = Intent(this, SerieSelectedActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
