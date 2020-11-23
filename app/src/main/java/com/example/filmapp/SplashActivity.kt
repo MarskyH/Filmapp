95d7d78db74272a4047872a6fd3bfe547e9fa4e6
Marcus Loureiro <marcushuriel08@hotmail.com>
2020-11-23 13:31:15 -0300

    Splash

--- app/src/main/java/com/example/filmapp/SplashActivity.kt
+++ app/src/main/java/com/example/filmapp/SplashActivity.kt
@@ -12,7 +12,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import com.example.filmapp.home.HomeActivity
 class SplashActivity: AppCompatActivity() {
     val scope = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
